7ea2a3e114160c9079e972acaa1e646fea6d8d54
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2022-11-07 10:34:51 +0200

    [syncstream] Add syncstream package to sync asynchronous streams
    Package sends message using sendFunc param in Send method and waits for specific
    message type is received. Usage:
    - send message: pass sendFunc and responseType parameters to Send API;
    - call ProcessMessages function for all received messages in different thread:
      if it is one of expected message, this message will be consumed and `true` is
      returned, otherwise ProcessMessages returns false;
    - consumed received message will be returned by corresponding blocking Send API.
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ utils/syncstream/syncstream_test.go
@@ -0,0 +1,65 @@
+package syncstream_test
+
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	"github.com/aoscloud/aos_common/utils/syncstream"
+)
+
+/***********************************************************************************************************************
+ * Tests
+ **********************************************************************************************************************/
+
+func TestSyncStream(t *testing.T) {
+	type testType1 struct {
+		i int
+	}
+
+	type testType2 struct {
+		s string
+	}
+
+	type testType3 struct {
+		f float64
+	}
+
+	testData := []interface{}{
+		&testType1{i: 42},
+		&testType2{s: "test"},
+		&testType3{f: 3.14},
+	}
+
+	testChannel := make(chan interface{}, 1)
+
+	stream := syncstream.New()
+
+	go func() {
+		for {
+			response, ok := <-testChannel
+			if !ok {
+				return
+			}
+
+			stream.ProcessMessages(response)
+		}
+	}()
+
+	for _, testItem := range testData {
+		response, err := stream.Send(context.Background(), func() error {
+			testChannel <- testItem
+
+			return nil
+		}, reflect.TypeOf(testItem))
+		if err != nil {
+			t.Errorf("Send stream error: %v", err)
+		}
+
+		if response != testItem {
+			t.Errorf("Wrong response received: %v", response)
+		}
+	}
+
+	close(testChannel)
+}
