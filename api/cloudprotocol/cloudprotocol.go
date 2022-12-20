6755cc2b0ac4e6de63276a1aeec0988a0bfd2886
Mykola Solianko <mykola_solianko@epam.com>
2022-12-20 12:35:03 +0200

    [cloudprotocol] Change fields in PushLog
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>

--- api/cloudprotocol/cloudprotocol.go
+++ api/cloudprotocol/cloudprotocol.go
@@ -407,12 +407,12 @@ type InstanceMonitoringData struct {
 
 // PushLog push service log structure.
 type PushLog struct {
-	NodeID    string `json:"nodeId"`
-	LogID     string `json:"logId"`
-	PartCount uint64 `json:"partCount,omitempty"`
-	Part      uint64 `json:"part,omitempty"`
-	Content   []byte `json:"content,omitempty"`
-	Error     string `json:"error,omitempty"`
+	NodeID     string    `json:"nodeId"`
+	LogID      string    `json:"logId"`
+	PartsCount uint64    `json:"partsCount,omitempty"`
+	Part       uint64    `json:"part,omitempty"`
+	Content    []byte    `json:"content,omitempty"`
+	ErrorInfo  ErrorInfo `json:"errorInfo,omitempty"`
 }
 
 // UnitStatus unit status structure.
