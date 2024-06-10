a3934d0864671b83bcca59aa32074d6a0f60c329
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2024-06-10 12:00:07 +0300

    [generate_api.sh] Add common.proto to generate API
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- generate_api.sh
+++ generate_api.sh
@@ -3,6 +3,11 @@
 # configuration
 
 OUT_DIR="api"
+PROTO_DIR=$1
+
+PACKAGE_PREFIX="github.com/aoscloud/aos_common/api"
+
+COMMON_SOURCES="common/v1/common.proto"
 
 IAM_SOURCES="iamanager/v5/iamanager.proto"
 
@@ -21,37 +26,35 @@ create_package_options() {
     go_opt=""
 
     for item in $1; do
-        go_opt+=" --go_opt=M${item}=./;$2"
+        opt="M${item}=${2}"
+        go_opt+=" --go_opt=${opt} --go-grpc_opt=${opt}"
     done
 
-    for item in $1; do
-        go_opt+=" --go-grpc_opt=M${item}=./;$2"
-    done
-
-    echo ${go_opt}
+    echo "${go_opt}"
 }
 
-COMMON_OPTIONS="--proto_path=${1} --go_out=${OUT_DIR} \
-    --go_opt=paths=source_relative --go-grpc_out=${OUT_DIR} --go-grpc_opt=paths=source_relative"
+create_protoc_options() {
+    go_opt=$(create_package_options "$1" "$2")
+
+    go_opt+=" --proto_path=${PROTO_DIR} --go_out=${OUT_DIR} --go-grpc_out=${OUT_DIR}"
+
+    echo "${go_opt}"
+}
 
 # clear output dir
 
 mkdir -p ${OUT_DIR}
+rm -rf ${OUT_DIR}/common ${OUT_DIR}/communicationmanager ${OUT_DIR}/iamanager ${OUT_DIR}/servicemanager ${OUT_DIR}/updatemanager
 
-rm -rf ${OUT_DIR}/communicationmanager ${OUT_DIR}/iamanager ${OUT_DIR}/servicemanager ${OUT_DIR}/updatemanager
+MODULE_COMMON_OPTS=$(create_package_options "${COMMON_SOURCES}" "${PACKAGE_PREFIX}/common")
 
+# Generate common services
+protoc $(create_protoc_options "${COMMON_SOURCES}" "./common") ${COMMON_SOURCES}
 # Generate IAM services
-
-protoc $COMMON_OPTIONS $(create_package_options "${IAM_SOURCES}" iamanager) ${IAM_SOURCES}
-
+protoc $(create_protoc_options "${IAM_SOURCES}" "./iamanager") ${MODULE_COMMON_OPTS} ${IAM_SOURCES}
 # Generate SM services
-
-protoc $COMMON_OPTIONS $(create_package_options "${SM_SOURCES}" servicemanager) ${SM_SOURCES}
-
+protoc $(create_protoc_options "${SM_SOURCES}" ./servicemanager) ${MODULE_COMMON_OPTS} ${SM_SOURCES}
 # Generate UM services
-
-protoc $COMMON_OPTIONS $(create_package_options "${UM_SOURCES}" updatemanager) ${UM_SOURCES}
-
+protoc $(create_protoc_options "${UM_SOURCES}" ./updatemanager) ${MODULE_COMMON_OPTS} ${UM_SOURCES}
 # Generate CM services
-
-protoc $COMMON_OPTIONS $(create_package_options "${CM_SOURCES}" communicationmanager) ${CM_SOURCES}
+protoc $(create_protoc_options "${CM_SOURCES}" ./communicationmanager) ${MODULE_COMMON_OPTS} ${CM_SOURCES}
